15acad14bc2cf28a7f910cfe22428ae2bc2050a4
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-11-11 14:47:40 -0300

    Put and delete

--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Tema } from '../model/Tema';
 
 @Injectable({
@@ -7,25 +7,37 @@ import { Tema } from '../model/Tema';
 })
 export class TemaService {
 
-
-
   constructor(private http: HttpClient) { }
 
   token = {
     headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
   }
 
+
   getAllTemas() {
-    return this.http.get('http://localhost:8080/tema',this.token)
+    return this.http.get('http://localhost:8080/tema', this.token)
   }
 
   getByIdTema(id: number) {
-    return this.http.get(`http://localhost:8080/tema/${id}`,this.token) //tem que ser crase para trazer o parametro certo
+    return this.http.get(`http://localhost:8080/tema/${id}`, this.token)
   }
 
   postTema(tema: Tema) {
     return this.http.post('http://localhost:8080/tema', tema, this.token)
   }
 
-  
+  putTema(tema: Tema) {
+    return this.http.put('http://localhost:8080/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
+  getByNomeTema(nome: string) {
+    return this.http.get(`http://localhost:8080/tema/nome/${nome}`, this.token)
+  }
+
+
 }
+
